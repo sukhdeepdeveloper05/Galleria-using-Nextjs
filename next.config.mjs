24c2fd2c5f569fa3c2501f9b99c06a3140278b53
sukhdeepsohal05 <sukhdeepsohal05@gmail.com>
2024-07-09 18:55:49 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
+        protocol: "https",
+        port: "",
         hostname: "images.unsplash.com",
       },
       {
+        protocol: "https",
+        port: "",
         hostname: "plus.unsplash.com",
       },
     ],
