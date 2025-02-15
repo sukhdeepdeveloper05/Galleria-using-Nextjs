96fdc13d75568deb524825198714000dffaa8f2b
Sukhdeep Developer <95998276+sukhdeepdeveloper05@users.noreply.github.com>
2025-02-15 17:52:53 +0530

    Update fetchData.js

--- src/lib/fetchData.js
+++ src/lib/fetchData.js
@@ -3,27 +3,27 @@
 import { apiKey } from "../config";
 
 export default async function fetchData(endpoint) {
-  try {
+  // try {
     const response = await fetch(`https://api.unsplash.comm${endpoint}`, {
       headers: {
         Authorization: `Client-ID ${apiKey}`,
       },
     });
 
-    // if (!response.ok) {
-    //   if (response.text() === "Rate Limit Exceeded") {
-    //     throw new Error(await response.text());
-    //   } else {
-    //     throw new Error(
-    //       (await response.json().errors[0]) || "Something went wrong"
-    //     );
-    //   }
-    // }
+    if (!response.ok) {
+      if (response.text() === "Rate Limit Exceeded") {
+        throw new Error(await response.text());
+      } else {
+        throw new Error(
+          (await response.json().errors[0]) || "Something went wrong"
+        );
+      }
+    }
 
     const resData = await response.json();
 
     return resData;
-  } catch (error) {
-    throw new Error(error.message);
-  }
+  // } catch (error) {
+  //   throw new Error(error.message);
+  // }
 }
