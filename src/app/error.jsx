646e8fe7f937f7b67284cfd7b399ccc7b5ecfc71
Sukhdeep Developer <95998276+sukhdeepdeveloper05@users.noreply.github.com>
2025-02-15 18:06:31 +0530

    Update error.jsx

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-export default function error({ error }) {
+export default function Error({ error }) {
   return (
     <main className="h-screen -mt-16 flex items-center flex-col justify-center">
       <h1>Something went wrong</h1>
